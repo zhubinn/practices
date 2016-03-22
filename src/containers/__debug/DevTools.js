99e2d98fe3ecfa5d24035d7ecb5e61caa3fb9700
C <4019980@qq.com>
2016-03-22 15:56:17 +0800

    fixbug 未安装redux devtools时，页面版devtools初始化失败

--- src/containers/__debug/DevTools.js
+++ src/containers/__debug/DevTools.js
@@ -6,22 +6,12 @@ import { createDevTools } from 'redux-devtools'
 import LogMonitor from 'redux-devtools-log-monitor'
 import DockMonitor from 'redux-devtools-dock-monitor'
 
-const monitorState = {
-    position: "left",
-    size: 16,
-    isVisible: false
-}
-
-class DevTools extends React.Component {
-    render() {
-        return (
-            <DockMonitor toggleVisibilityKey="ctrl-h"
-                         changePositionKey="ctrl-w"
-                         monitorState={monitorState}>
-                <LogMonitor/>
-            </DockMonitor>
-        )
-    }
-}
+const DevTools = createDevTools(
+    <DockMonitor toggleVisibilityKey='ctrl-h'
+                 changePositionKey='ctrl-q'
+                 changeMonitorKey='ctrl-m'>
+        <LogMonitor/>
+    </DockMonitor>
+)
 
 export default connect()(DevTools)
