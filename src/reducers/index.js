dffd705d8aed9b5fcc299db6a20d24ef12a59fe1
leesx <1028315063@qq.com>
2016-04-14 15:06:59 +0800

    conflict

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,8 @@
 import { combineReducers } from 'redux'
 import { routerReducer as routing } from 'react-router-redux'
 
-<<<<<<< HEAD
+import components from './components'
+import __demo from './__demo'
 import report from './__demo/report'
 import user from './__demo/user'
 import hello from './__demo/hello'
@@ -12,18 +13,10 @@ import customEditField from './__demo/customEditField'
 import numberReportViewState from './numberReport/numberReportView'
 import reportOther from './report/index'
 
-const rootReducer = combineReducers({
-	reportOther,
-    report,
-    user,
-=======
-import components from './components'
-import __demo from './__demo'
 
 const rootReducer = combineReducers({
     components,
     __demo,
->>>>>>> edea6834b571935104b73013b17a1c07db74ebc5
     routing,
     hello,
     customEditField,
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,7 @@
  */
 import { Route, IndexRoute } from 'react-router'
 
-<<<<<<< HEAD:src/routes.js
-import IndexPage from 'containers/IndexPage'
-import NoMatch from 'containers/NoMatch'
-import DemoTablePage from 'containers/__demo/TablePage'
-import DemoLoginPage from 'containers/__demo/LoginPage'
+
 
 import HelloPage from 'containers/__demo/HelloPage'
 import CustomEditFieldPage from 'containers/__demo/CustomEditFieldPage'
@@ -15,12 +11,6 @@ import NumberReportViewPage from 'containers/numberReport/NumberReportViewPage'
 // import DemoPagination from 'containers/__demo/Pagination'
 import DemoReportListPage from 'containers/report/DemoReportListPage'
 
-
-import DemoAutoCompletePage from 'containers/__demo/AutoCompletePage'
-import DemoDatePickerPage from 'containers/__demo/DatePickerPage'
-import DepToTreePage from 'containers/__demo/DepToTreePage'
-import DemoModalPage from 'containers/__demo/ModalPage'
-=======
 import MasterPage from 'containers/Master/Default'
 import DemoTablePage from 'containers/__demo/Table'
 import DemoLoginPage from 'containers/__demo/Login'
@@ -29,36 +19,27 @@ import DemoDatePickerPage from 'containers/__demo/DatePicker'
 import DepToTreePage from 'containers/__demo/DepToTree'
 import DemoModalPage from 'containers/__demo/Modal'
 import Error_404 from 'containers/Error/404'
->>>>>>> edea6834b571935104b73013b17a1c07db74ebc5:src/routes/index.js
-
 
 export default (
-    <Route path="/" component={MasterPage}>
-        <Route path="__demo">
-            <Route path="table" component={DemoTablePage}/>
-            <Route path="login" component={DemoLoginPage}/>
-            <Route path="hello" component={HelloPage}/>
-            <Route path="customEditFieldPage" component={CustomEditFieldPage}/>
+<Route path="/" component={MasterPage}>
+    <Route path="__demo">
+    <Route path="table" component={DemoTablePage}/>
+    <Route path="login" component={DemoLoginPage}/>
+    <Route path="hello" component={HelloPage}/>
+    <Route path="customEditFieldPage" component={CustomEditFieldPage}/>
 
 
-            <Route path="autocomplete" component={DemoAutoCompletePage}/>
-            <Route path="datepicker" component={DemoDatePickerPage}/>
-            <Route path="daterange" component={DemoDatePickerPage}/>
-            <Route path="deptotree" component={DepToTreePage}/>
-            <Route path="modal" component={DemoModalPage}/>
+    <Route path="autocomplete" component={DemoAutoCompletePage}/>
+    <Route path="datepicker" component={DemoDatePickerPage}/>
+    <Route path="daterange" component={DemoDatePickerPage}/>
+    <Route path="deptotree" component={DepToTreePage}/>
+    <Route path="modal" component={DemoModalPage}/>
 
-        </Route>
-<<<<<<< HEAD:src/routes.js
-        <Route path="numberReport">
-            <Route path="numberReportViewPage" component={NumberReportViewPage}/>
-        </Route>
-        /*<Route path="report">
-            <Route path="list" component={DemoReportListPage}/>
-        </Route>*/
+    </Route>
+    <Route path="numberReport">
+    <Route path="numberReportViewPage" component={NumberReportViewPage}/>
+    </Route>
+    <Route path="*" component={Error_404}/>
 
-        <Route path="*" component={NoMatch}/>
-=======
-        <Route path="*" component={Error_404}/>
->>>>>>> edea6834b571935104b73013b17a1c07db74ebc5:src/routes/index.js
     </Route>
 )
