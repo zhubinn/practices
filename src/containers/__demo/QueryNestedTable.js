140d115ad2acbaa231d06709dafb86e04d72997d
janeluck <janeluck158@gmail.com>
2016-04-26 09:49:45 +0800

    demo_table

--- src/containers/__demo/QueryNestedTable.js
+++ src/containers/__demo/QueryNestedTable.js
@@ -8,57 +8,19 @@ import INPUTTYPE from 'components/QueryNestedTable/inputType'
 import { updateDataSource, updateChildDataSource } from 'actions/__demo/queryNestedTable'
 import 'antd/style/index.less'
 
-const columns = [{
-    title: '姓名',
-    dataIndex: 'name',
-    key: 'name',
-    inputType: INPUTTYPE.DATE,
-}, {
-    title: '年龄',
-    dataIndex: 'age',
-    key: 'age',
-    inputType: INPUTTYPE.STRING,
-}, {
-    title: '住址',
-    dataIndex: 'address',
-    key: 'address',
-    inputType: INPUTTYPE.NUMBER,
-}];
 
 class QueryNestedTablePage extends React.Component {
     constructor() {
         super()
-        this.updateDataSource = this.updateDataSource.bind(this)
-    }
 
-    updateDataSource() {
-        this.props.updateDataSource(columns, columns)
     }
 
     render() {
-        const { updateChildDataSource } = this.props
-        const {
-            dataSource,
-            childProps,
-            showSearchTable,
-            } = this.props.$$QueryNestedTable.toJS()
+
 
         return (
             <div>
-                <Breadcrumb>
-                    <Breadcrumb.Item>客户</Breadcrumb.Item>
-                    <Breadcrumb.Item href="">客户列表</Breadcrumb.Item>
-                </Breadcrumb>
-                <QueryNestedTable
-                    showSearchTable={showSearchTable}
-                    dataSource={dataSource}
-                    columns={columns}
-                    childProps={childProps}
-                    size="middle"
-                    bordered
-                    updateDataSource={this.updateDataSource}
-                    updateChildDataSource={updateChildDataSource}
-                />
+
             </div>
         )
     }
